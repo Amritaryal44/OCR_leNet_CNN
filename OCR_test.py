d8ca77b6135b560cf1826da81c3d494201e30b3f
amrit <amritaryal44@gmail.com>
2020-04-23 21:50:46 +0545

    successfully tested webcam

--- OCR_test.py
+++ OCR_test.py
@@ -26,16 +26,15 @@ while True:
     img = np.asarray(imgOriginal)
     img = cv2.resize(img, (32,32))
     img = preprocessing(img)
-    #img = img.reshape(1,32,32,1)
-    cv2.imshow("processed", img)
-    """#predict
+    img = img.reshape(1,32,32,1)
+    #predict
     classIndex = int(model.predict_classes(img))
     predictions = model.predict(img)
     probVal = np.amax(predictions)
 
     if probVal>threshold:
         cv2.putText(imgOriginal, str(classIndex)+" "+str(probVal), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 1)
-    cv2.imshow("Original Image", imgOriginal)"""
+    cv2.imshow("Original Image", imgOriginal)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
